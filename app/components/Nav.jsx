fa8fab87a8c6c4d8930893e0e54e54a9cc2a848c
Vaibhav <vaibhav.verma2697@gmail.com>
2018-03-06 13:15:56 +0530

    Added naviagation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,10 +8,10 @@ var Nav=React.createClass({
                                         <ul className="menu">
                                                 <li className="menu-text">React Timer </li>
                                                 <li>
-                                                        <IndexLink to='/' activeClassName="active-Link">Timer</IndexLink>
+                                                        <IndexLink to='/' activeClassName="active-link">Timer</IndexLink>
                                                 </li>
                                                 <li>
-                                                        <Link to="/" activeClassName="active-Link">Countdown</Link>
+                                                        <Link to="/countdown" activeClassName="active-link">Countdown</Link>
                                                 </li>
                                         </ul>
                                 </div>
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var Timer=React.createClass({
+        render:function(){
+                return(
+                        <div>
+                                <h1>Timer Component</h1>
+                        </div>
+                );
+        }
+});
+
+module.exports=Timer;
